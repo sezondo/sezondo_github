01c3a0920fc6f8bac465a14cdbdde3a7afadf1ea
sezondo <psdd205050@gmail.com>
2025-05-09 00:40:34 +0900

    재귀함수 공부

--- study/homework/study.cpp
+++ study/homework/study.cpp
@@ -1,21 +1,20 @@
 #include<stdio.h>
+#include<iostream>
 
-int ggdan();
+void temp(int n){
+    if(n <= 0)return;
+    temp(n/2);
+    std::cout << n << " "; 
+    
+}
+
 
 int main(){
 
-    int num[1];
-    
-    scanf("%d%d",&num[0], &num[1]);
-    ggdan(&num[0], num[1]);
+    int a;
+    std::cin >> a;
+    temp(a);
 
     return 0;
 }
 
-int ggdan(int x, int y) {
-
-    for (int i = 1; i <= 9; i++)
-    {
-        printf("%d * %d = %d\n", x, i, x * i);
-    }
-}
---
+++ study/homework/studystudyt/studystudyt/studystudyt.cpp
@@ -0,0 +1,34 @@
+﻿#include<stdio.h>
+#include<iostream>
+
+//형성평가 6번
+int MUL = 1;
+
+void num(int n) {
+
+    if (n <= 0)
+    {
+        return;
+    }
+    if (n % 10 != 0)
+    {
+        MUL *= ( n % 10);
+    }
+    
+    n /= 10;
+    num(n);
+}
+
+int main() {
+    int num1,num2,num3;
+    int mul;
+    std::cin >> num1 >> num2 >> num3;
+
+    mul = num1 * num2 * num3;
+    num(mul);
+
+    std::cout << MUL << std::endl;
+
+    return 0;
+}
+
